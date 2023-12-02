39481d03c8bf867c94b97f5e528414da609330d8
mohamed12samy <mohamedsamyabbas99@gmail.com>
2023-12-03 00:41:35 +0200

    pdf

--- components/PDFComponent.js
+++ components/PDFComponent.js
@@ -3,103 +3,108 @@ import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/render
 
 // Create styles
 
-const styles = StyleSheet.create({ 
-    container: { 
-     }, 
-    header: { 
-      display: 'flex', 
-      justifyContent: 'space-between',
-       marginBottom: 10, 
-      padding:30,
-      }, 
-    logo: { 
-      display: 'flex', 
-      color:'black',
-      fontWeight: 900,
-      alignSelf:'left',
-      fontSize:'50px',
-      backgroundColor:'blue'
-    },
-    title: { 
-      display: 'flex', 
-      textAlign: 'right', 
-      fontSize: 40, 
-      fontWeight: 900, 
-      backgroundColor:'yellow',
-      alignSelf:'right',
-    },
-    table: { 
-      width: '100%', 
-      borderCollapse: 'collapse', }, 
-    tableCell: { 
-        border: '1px solid black', 
-        padding: 10, 
-        textAlign: 'center', }, 
-    tableRow: { 
-      backgroundColor: '#d9f2d9', }, 
-    footer: { 
-      display: 'flex', 
-      alignItems: 'center', 
-      justifyContent: 'space-between', 
-      marginTop: 20, }, 
-    definition:{
-      width:'100%',
-      alignContent:'right',
-      alignItems:'right',
-      fontSize:20,
-      padding:30
-      },
-    total: { fontSize: 18, fontWeight: 'bold', }, });
-  
+const styles = StyleSheet.create({
+  page: {
+    fontFamily: "Helvetica",
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    lineHeight: 1.5,
+    flexDirection: "column",
+  },
+  logo: {
+    width: 74,
+    height: 66,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  invoiceNoContainer: {
+    flexDirection: "row",
+    marginTop: 36,
+    justifyContent: "flex-end",
+  },
+  invoiceDateContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  invoiceDate: {
+    fontSize: 12,
+    fontStyle: "bold",
+  },
+  label: {
+    width: 60,
+  },
+});
 
 // Create Document Component
 export const PDFDocument = (props)=>{
-    const {yah} = props;
-    console.log(yah.customerName
-      )
+    const {yah, ref} = props;
+    
     return (
+      <div size="A4" style={styles.page} ref={ref}>
+        
+        <div>
+        <div style={styles.invoiceNoContainer}>
+          <p style={styles.label}>Invoice No:</p>
+          <p style={styles.invoiceDate}>{"NO."}</p>
+        </div>
+        <div style={styles.invoiceDateContainer}>
+          <p style={styles.label}>Date: </p>
+          <p>{"Date"}</p>
+        </div>
+      </div>
+        
+       
+      </div>
 
-        <Document> 
-        <Page size="A4" style={styles.container}> 
-          <View style={styles.header}> 
-            <Text style={styles.logo}>YAH</Text> 
-            <Text style={styles.title}>aasasas</Text> 
 
-          </View> 
-          {/* <View style={styles.definition}> 
-            <Text >mo2asaset wa2t el ta7lya lel tegara</Text> 
-            <Text >el ra2m el dareeby: 301053363100003</Text> 
-            <Text>Roshn front</Text> 
-            <Text>Start up hub</Text> 
-            <Text>Riyadh, 13326</Text> 
-          </View>  */}
-          <View style={styles.definition}> 
-            <Text >customer name: {yah.customerName}</Text> 
-            <Text >product name: {yah.productName}</Text> 
-            <Text >price: {yah.price}</Text> 
-            <Text >quantity: {yah.quantity}</Text> 
-            <Text >billing type: {yah.billType}</Text> 
-            <Text >total Price: {yah.price * yah.quantity}</Text> 
-            <Text >date: {yah.date}</Text> 
-          </View> 
+
+
+
+
+
+      //   <Document> 
+      //   <Page size="A4" style={styles.container}> 
+      //     <View style={styles.header}> 
+      //       <Text style={styles.logo}>YAH</Text> 
+      //       <Text style={styles.title}>aasasas</Text> 
+
+      //     </View> 
+      //     {/* <View style={styles.definition}> 
+      //       <Text >mo2asaset wa2t el ta7lya lel tegara</Text> 
+      //       <Text >el ra2m el dareeby: 301053363100003</Text> 
+      //       <Text>Roshn front</Text> 
+      //       <Text>Start up hub</Text> 
+      //       <Text>Riyadh, 13326</Text> 
+      //     </View>  */}
+      //     <View style={styles.definition}> 
+      //       <Text >customer name: {yah.customerName}</Text> 
+      //       <Text >product name: {yah.productName}</Text> 
+      //       <Text >price: {yah.price}</Text> 
+      //       <Text >quantity: {yah.quantity}</Text> 
+      //       <Text >billing type: {yah.billType}</Text> 
+      //       <Text >total Price: {yah.price * yah.quantity}</Text> 
+      //       <Text >date: {yah.date}</Text> 
+      //     </View> 
 
          
-          <View style={styles.table}> 
-            <View style={styles.tableRow}> 
-            <Text style={styles.tableCell}>Start</Text> 
-            <Text style={styles.tableCell}>End</Text> 
-            <Text style={styles.tableCell}>Total</Text> 
-          </View> 
-          <View> 
-            <Text style={styles.tableCell}>28/11/2021</Text> 
-            <Text style={styles.tableCell}>30/11/2021</Text> 
-            <Text style={styles.tableCell}>149.99 SAR</Text> 
-          </View> 
-          </View> 
-          <View style={styles.footer}> 
-            <Text style={styles.total}>Total: 149.99 SAR</Text> 
-            <Text>YAH{'\n'}Riyadh 12333{'\n'}Saudi Arabia</Text> 
-          </View> 
-        </Page> 
-      </Document>
+      //     <View style={styles.table}> 
+      //       <View style={styles.tableRow}> 
+      //       <Text style={styles.tableCell}>Start</Text> 
+      //       <Text style={styles.tableCell}>End</Text> 
+      //       <Text style={styles.tableCell}>Total</Text> 
+      //     </View> 
+      //     <View> 
+      //       <Text style={styles.tableCell}>28/11/2021</Text> 
+      //       <Text style={styles.tableCell}>30/11/2021</Text> 
+      //       <Text style={styles.tableCell}>149.99 SAR</Text> 
+      //     </View> 
+      //     </View> 
+      //     <View style={styles.footer}> 
+      //       <Text style={styles.total}>Total: 149.99 SAR</Text> 
+      //       <Text>YAH{'\n'}Riyadh 12333{'\n'}Saudi Arabia</Text> 
+      //     </View> 
+      //   </Page> 
+      // </Document>
 )};
--- pages/CalibrationCertificate.js
+++ pages/CalibrationCertificate.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import * as FileSaver from "file-saver";
 import XLSX from "sheetjs-style";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCalibrationForm } from "redux/mineSlicer";
  import {PDFDocument} from '../components/PDFComponent'
 import { Document, Page, Text, View,Font, StyleSheet,Image, PDFDownloadLink} from '@react-pdf/renderer';
+import jsPDF from 'jspdf';
+import '../assets/fonts/Amiri.js'; 
+import autoTable from 'jspdf-autotable'
 
 const CalibrationCertificate = () => {
    const [customerName, setCustomerName] = useState("");
@@ -13,6 +16,115 @@ const CalibrationCertificate = () => {
   const [price, setPrice] = useState("");
   const [quantity, setQuantity] = useState("");
   const [date, setDate] = useState("");
+const ref = useRef();
+  const generatePDF = () => {
+    const doc = new jsPDF();
+     // set the font to Amiri
+    const arabic = 'مرحبا';
+    const arabicWithDiacritics = 'مَرْحَبًا';
+    doc.setFont('Amiri-Regular');
+    doc.setFontSize(30);
+    doc.text('YAH', 30, 40, { align: 'left' }); // align the text to the right
+    doc.text('فاتورة ضريبية', 200, 40, { align: 'right' }); // align the text to the right
+    doc.setFontSize(20);
+    doc.text(` :فاتورة الى`, 200, 70, { align: 'right' }); // align the text to the right
+    doc.setFontSize(15);
+    doc.text(`${customerName} `, 200, 80, { align: 'right'}); // align the text to the right
+
+    doc.setFontSize(20);
+    doc.text("رقم الفاتورة  ", 70, 70, { align: 'right' }); // align the text to the right
+    doc.setFontSize(20);
+    doc.text("تاريخ الفاتورة   ", 70, 80, { align: 'right'}); // align the text to the right
+    doc.setFontSize(15);
+    doc.text(new Date(date).toLocaleDateString(), 29, 80, { align: 'right'}); // align the text to the right
+
+
+
+    const margin = 0.5; // inches on a 8.5 x 11 inch sheet.
+    const verticalOffset = margin;
+    var columns = [
+        {title: "اسم المنتج", dataKey: "col1"},
+        {title: "السعر", dataKey: "col2"},
+        {title: "العدد", dataKey: "col3"},
+        {title: "المجموع", dataKey: "col4"},
+    ];
+    var rows = [
+      {
+        "col1": productName, 
+        "col2": price, 
+        "col3": quantity,
+        "col4": quantity * price
+      }
+    ];
+
+    autoTable(doc, {
+      head: [['المجموع ', 'العدد', 'السعر', 'اسم المنتج']],
+      body: [
+        [quantity*price, quantity, price, productName]
+      ],
+      startY:100,
+      styles: { overflow: 'linebreak', fontSize: 12, font: 'Amiri-Regular', lineColor:'black', lineWidth:1 },
+      bodyStyles:{halign:'right', fillColor:[255,255,255], fontSize:20, textColor:'black', lineColor:'black',lineWidth:1},
+      headStyles:{halign:'right', fillColor:[201,203,207], fontSize:18, textColor:'black', lineColor:'black',lineWidth:1},
+    })
+
+    doc.save('a4.pdf'); // save the document as a file
+   
+    // doc.html(ref.current, {
+    //   async callback(docc) {
+
+    //     const arabic = 'مرحبا';
+    //     const arabicWithDiacritics = 'مَرْحَبًا';
+    //     docc.setFont('Amiri-Regular');
+    //     docc.setFontSize(40);
+
+
+
+    //     docc.text(arabic, 200, 40, { align: 'right' }); // align the text to the right
+    //     docc.text(arabicWithDiacritics, 200, 80, { align: 'right' });
+    //     docc.save("pdf_name.pdf");
+    //   }
+    // });
+    
+    
+    
+
+  };
+  const styles = StyleSheet.create({
+    page: {
+      fontFamily: "Helvetica",
+      fontSize: 11,
+      paddingTop: 30,
+      paddingLeft: 60,
+      paddingRight: 60,
+      lineHeight: 1.5,
+      flexDirection: "column",
+    
+    },
+    logo: {
+      width: 74,
+      height: 66,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    invoiceNoContainer: {
+      flexDirection: "row",
+      marginTop: 36,
+      justifyContent: "flex-end",
+    },
+    invoiceDateContainer: {
+      flexDirection: "row",
+      justifyContent: "flex-end",
+    },
+    invoiceDate: {
+      fontSize: 12,
+      fontStyle: "bold",
+    },
+    label: {
+      width: 60,
+    },
+  });
+
 
   return (
     <div className="">
@@ -29,59 +141,59 @@ const CalibrationCertificate = () => {
           
             <div className="mt-2">
               <label
-                className="block text-sm text-gray-00"
+                className="block text-sm text-gray-00 text-right"
                 htmlFor="Customer Name"
               >
-                Customer Name
+                اسم العميل
               </label>
               <input
-                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded text-right"
                 id="Customer Name"
                 name="Customer Name"
                 type="text"
                 autoComplete="off"
                 onChange={(e) => setCustomerName(e.target.value)}
                 required
-                placeholder="Customer Name"
+                placeholder="اسم العميل"
                 aria-label="Customer Name"
               />
             </div>
             <div className="mt-2">
               <label
-                className="block text-sm text-gray-00"
+                className="block text-sm text-gray-00 text-right"
                 htmlFor="Product Name"
               >
-                Product Name
+                اسم المنتج
               </label>
               <input
-                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded text-right"
                 id="Product Name"
                 name="Product Name"
                 type="text"
                 autoComplete="off"
                 onChange={(e) => setProductName(e.target.value)}
                 required
-                placeholder="Product Name"
+                placeholder="اسم المنتج"
                 aria-label="Product Name"
               />
             </div>
             <div className="mt-2">
-              <label className="block text-sm text-gray-00" htmlFor="W.O.No.">
-                Bill Type
+              <label className="block text-sm text-gray-00 text-right" htmlFor="W.O.No.">
+                نوع الفاتورة
               </label>
              
                 <div className="relative">
                   <select
-                    className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded"
+                    className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded text-right"
                     id="grid-state"
                     value={billType}
                     onChange={(e) => setBillType(e.target.value)}
                     defaultValue={"purchaseInvoice"}
                   >
-                    <option value={"purchaseInvoice"}>Purchase invoice </option>
-                    <option value={"priceList"}>Price list</option>
+                    <option value={"purchaseInvoice"}>فاتورة مشتريات </option>
+                    <option value={"priceList"}>عرض اسعار</option>
                   </select>
-                  <div className="pointer-events-none absolute left-96 pl-20 bottom-4 pin-y pin-r flex items-center px-2 text-grey-darker">
+                  <div className="pointer-events-none absolute left-96 pl-20 bottom-4 pin-y pin-r flex items-center px-2 text-grey-darker text-right">
                     <svg
                       className="h-4 w-4"
                       xmlns="http://www.w3.org/2000/svg"
@@ -95,84 +207,75 @@ const CalibrationCertificate = () => {
             </div>
             <div className="mt-2">
               <label
-                className="block text-sm text-gray-00"
+                className="block text-sm text-gray-00 text-right"
                 htmlFor="Price"
               >
-                Price
+                السعر
               </label>
               <input
-                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded text-right"
                 id="Price"
                 name="Price"
-                type="text"
+                type="number"
                 autoComplete="off"
                 onChange={(e) => setPrice(e.target.value)}
                 required
-                placeholder="Price"
+                placeholder="السعر"
                 aria-label="Price"
               />
             </div>
             <div className="mt-2">
               <label
-                className="block text-sm text-gray-00"
+                className="block text-sm text-gray-00 text-right"
                 htmlFor="Quantity"
               >
-                Quantity
+                العدد
               </label>
               <input
-                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded text-right"
                 id="Quantity"
                 name="Quantity"
-                type="text"
+                type="number"
                 autoComplete="off"
                 onChange={(e) => setQuantity(e.target.value)}
                 required
-                placeholder="Quantity"
+                placeholder="العدد"
                 aria-label="Quantity"
               />
             </div>
 
             <div className="mt-2">
               <label
-                className="block text-sm text-gray-00"
+                className="block text-sm text-gray-00 text-right"
                 htmlFor="date"
               >
-                Date
+                التاريخ
               </label>
               <input
-                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded text-right"
                 id="Date"
                 name="Date"
                 type="date"
                 autoComplete="off"
                 onChange={(e) => setDate(e.target.value)}
                 required
-                placeholder="Date"
+                placeholder="التاريخ"
                 aria-label="Date"
               />
             </div>
          
          
             <div className="mt-4">
-              {/* <button
-                className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
+               <button
+                className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded text-right"
                 type="button"
+                onClick={()=>{generatePDF()}}
               >
-                <PDFDownloadLink document={<PDFDocument  yah={customerName}
-                />} fileName={`YAHInvoice_${new Date().toLocaleDateString()}.pdf`}>
-                  {({ blob, url, loading, error }) =>
-                    loading ? 'Loading document...' : 'Export PDF'
-                  }
-              </PDFDownloadLink>
-              </button> */}
-              <PDFDownloadLink document={<PDFDocument yah={{customerName, productName, price, quantity, billType, date}}/>} 
-                fileName={`YAHInvoice_${new Date().toLocaleDateString()}.pdf`}>
-                  {({ blob, url, loading, error }) =>
-                    loading ? 'Loading document...' : 'Export PDF'
-                  }
-              </PDFDownloadLink>
+               استخرج الفاتورة
+              </button> 
+             
             </div>
-          </form>
+                     </form>
         </div>
       </main>
     </div>
